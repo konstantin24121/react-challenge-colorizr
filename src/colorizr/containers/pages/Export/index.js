9ee32ae8dd85f2947cbe373d8fb5bb8963ab07f4
VinstonWolf <konstantin24121@gmail.com>
2016-09-06 22:30:15 +0500

    кажется что сделал мало. Так оно и есть

--- src/colorizr/containers/pages/Export/index.js
+++ src/colorizr/containers/pages/Export/index.js
@@ -1,21 +1,56 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import tinycolor from 'tinycolor2';
 
 import MainLayout from '@common/layout/Main';
+import {Table} from 'react-bootstrap';
 
-export default class ExportPage extends React.Component {
+class ExportPage extends React.Component {
 
 	constructor(props) {
 		super(props);
 	}
 
 	render() {
+		let ColorRow = this.props.selectedColors.map(function(item, key){
+			var color = tinycolor(item);
+	
+			return (
+				<tr>
+					<td style={{backgroundColor: item}}></td>
+					<td>{item}</td>
+					<td>#{color.toHex().toUpperCase()}</td>
+					<td></td>
+				</tr>
+			)
+		})
 		return (
 			<MainLayout>
-				<div>Export page</div>
+				<h1 className="text-center">Customize and Export colors for Sass, Less or Stylus</h1>
+				<Table striped bordered condensed hover>
+					<thead>
+						<tr>
+							<th>Color</th>
+							<th>HEX</th>
+							<th>RGB</th>
+							<th>Variable name</th>
+						</tr>
+					</thead>
+					<tbody>
+						{ColorRow}
+					</tbody>
+				</Table>
 			</MainLayout>
 		);
 	}
 }
 
+function mapStateToProps(state){
+	return {
+		selectedColors: state.palete.get('colors')
+	}
+}
+
+export default connect(mapStateToProps)(ExportPage)
 
 
