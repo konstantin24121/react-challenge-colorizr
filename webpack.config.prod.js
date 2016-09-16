996dd5c99385bf6f37e6f7d9cae2723f79b0a501
Konstantin <konstantin24121@gmail.com>
2016-09-16 12:52:28 +0500

    от так правильно

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -11,7 +11,7 @@ module.exports = {
 	output: {
 		path: path.join(__dirname, 'public'),
 		filename: 'bundle.js',
-		publicPath: '/public/'
+		publicPath: '/'
 	},
 
 	plugins: [
@@ -46,8 +46,8 @@ module.exports = {
 			test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
 			loader: 'url-loader?limit=10000&mimetype=application/font-woff'
 		}, {
-			test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-			loader: 'file-loader'
+			test: /\.(ttf|eot|svg|woff|woff2)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+			loader: 'file-loader?name=fonts/[hash].[ext]'
 		}]
 	},
 	sassLoader: {
